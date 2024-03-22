0e89a8d92d9fc6a0d613a739bb1cdcd72facbced
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2024-03-22 10:39:50 -0500

    Edit spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require 'hyrax/specs/shared_specs/factories/strategies/json_strategy'
 require 'hyrax/specs/shared_specs/factories/strategies/valkyrie_resource'
 require 'coveralls'
 
-
 Coveralls.wear!('rails')
 
 FactoryBot.register_strategy(:valkyrie_create, ValkyrieCreateStrategy)
