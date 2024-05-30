636f9468790f76003df5aaa2eee18182079920c6
Max <mmayhew@emory.edu>
2024-05-30 14:42:48 -0400

    initial setup for field behaviors for publication form

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -1,12 +1,152 @@
 // wait for page to load (ruby seems to act weird if we don't)
 document.addEventListener('DOMContentLoaded', function (event) {
+
     // get accordion elements
     var x = document.getElementsByClassName("pub-form-header");
 
     for (i = 0; i < x.length; i++) {
         x[i].addEventListener("click", function () {
             var child = this.nextElementSibling;
-            child.classList.toggle("open");
+            if (child.className == "card-body open") {
+                child.className = "card-body"
+            } else {
+                child.className = "card-body open"
+            }
         })
+    }
+
+    //article fields
+    var confName = document.getElementsByClassName("form-group string optional publication_conference_name");
+    var isbn = document.getElementsByClassName("form-group string optional publication_isbn");
+    var series = document.getElementsByClassName("form-group string optional publication_series_title");
+    var edition = document.getElementsByClassName("form-group string optional publication_edition");
+    var publisherVersion = document.querySelector("label.control-label.select.optional");
+
+    //initial check on load for existing works
+    var p = document.getElementById("publication_content_genre");
+    if (p != null) {
+        var selectedValue = p.selectedOptions[0].label;
+        switch (selectedValue) {
+            case 'Article':
+                resetForm();
+                console.log("Chose: Article");
+                confName[0].style.display = "none";
+                isbn[0].style.display = "none";
+                series[0].style.display = "none";
+                edition[0].style.display = "none";
+                publisherVersion.insertAdjacentHTML("afterend", '&nbsp;<span id="pubform-pubver" class="badge badge-info required-tag">required</span>');
+
+                // validateForm();
+
+                break;
+
+            case 'Book':
+                resetForm();
+                console.log("Chose: Book");
+                break;
+
+            case 'Book Chapter':
+                resetForm();
+                console.log('Selected: Book Chapter');
+                break;
+
+            case 'Conference Paper':
+                resetForm();
+                console.log('Selected: Conference Paper');
+                break;
+
+            case 'Poster':
+                resetForm();
+                console.log('Selected: Poster');
+                break;
+
+            case 'Presentation':
+                resetForm();
+                console.log('Selected: Presentation');
+                break;
+
+            case 'Report':
+                resetForm();
+                console.log('Selected: Report');
+                break;
+
+            case ' ':
+                resetForm();
+                console.log('none selected');
+                break;
+        }
+    }
+
+    //get select element and watch if it changes
+    var e = document.getElementById("publication_content_genre");
+    e.addEventListener("change", function () {
+        var selectedValue = e.selectedOptions[0].label;
+
+        switch (selectedValue) {
+            case 'Article':
+                resetForm();
+                console.log("Chose: Article");
+                confName[0].style.display = "none";
+                isbn[0].style.display = "none";
+                series[0].style.display = "none";
+                edition[0].style.display = "none";
+                publisherVersion.insertAdjacentHTML("afterend", '&nbsp;<span id="pubform-pubver" class="badge badge-info required-tag">required</span>');
+
+                // validateForm();
+
+                break;
+
+            case 'Book':
+                resetForm();
+                console.log("Chose: Book");
+                break;
+
+            case 'Book Chapter':
+                resetForm();
+                console.log('Selected: Book Chapter');
+                break;
+
+            case 'Conference Paper':
+                resetForm();
+                console.log('Selected: Conference Paper');
+                break;
+
+            case 'Poster':
+                resetForm();
+                console.log('Selected: Poster');
+                break;
+
+            case 'Presentation':
+                resetForm();
+                console.log('Selected: Presentation');
+                break;
+
+            case 'Report':
+                resetForm();
+                console.log('Selected: Report');
+                break;
+
+            case ' ':
+                resetForm();
+                console.log('none selected');
+                break;
+        }
+
+    });
+
+
+    function resetForm() {
+        //reset the form at the beginning every time we switch
+
+        //article reset
+        console.log('RESET FORM!');
+        confName[0].style.display = "block";
+        isbn[0].style.display = "block";
+        series[0].style.display = "block";
+        edition[0].style.display = "block";
+        var pubform = document.getElementById("pubform-pubver");
+        if (pubform) { pubform.remove(); }
+
+
     }
 });
