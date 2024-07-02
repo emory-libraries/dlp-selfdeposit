cd710085d569f070161f661126150b7c9fa7d85c
Alex Zotov <alexandr.zotov@emory.edu>
2024-07-02 10:25:52 -0500

    debugging redirect for alternate_ids

--- app/controllers/hyrax/purl_controller.rb
+++ app/controllers/hyrax/purl_controller.rb
@@ -3,11 +3,12 @@ class PurlController < ApplicationController
   def redirect_to_original
     solr_object = Hyrax.custom_queries.find_by_alternate_id(alternate_ids: params[:alternate_ids].first)
     if solr_object.has_model.present? && solr_object.has_model.first == 'Publication'
-      redirect_to work_path(work)
+      object_url = "#{request.base_url}/concern/publications/#{solr_object.id}"
     elsif solr_object.respond_to?(:collection_type_gid) && solr_object.collection_type_gid.present?
-      redirect_to collection_path(work)
+      object_url = "#{request.base_url}/collections/#{solr_object.id}"
     else
       render plain: 'Item not found', status: :not_found
     end
+    redirect_to object_url
   end
 end
--- app/services/self_deposit/custom_queries/find_by_alternate_id.rb
+++ app/services/self_deposit/custom_queries/find_by_alternate_id.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 module SelfDeposit
   module CustomQueries
-    # @example
-    #   alternate_id = 'jhqsdh-emory'
-    #
-    #   Hyrax.custom_queries.find_by_alternate_id(alternate_ids:)
-    #
-    # @see https://github.com/samvera/valkyrie/wiki/Queries#custom-queries
     class FindByAlternateId
       def self.queries
         [:find_by_alternate_id]
@@ -19,24 +13,16 @@ module SelfDeposit
 
       attr_reader :query_service
 
-      ##
-      # @param alternate_id for a Publication, Collection
-      #
-      # @return Publication or Collection
       def find_by_alternate_id(alternate_ids:)
         @alternate_id = alternate_ids
         raise ::Valkyrie::Persistence::ObjectNotFoundError unless resource
         @query_service.find_by(alternate_ids: resource['alternate_ids'])
       end
 
-      # Queries Solr for a document that matches the provided alternate_id
-      # @yield [Publication or Collection]
       def resource
         @connection.get("select", params: { q: query, fl: "*", rows: 1 })["response"]["docs"].first
       end
 
-      # Solr query for for a Publication or Collaction with a alternate_ids_ssim that matches the provided id
-      # @return [Hash]
       def query
         "alternate_ids_ssim:#{@alternate_id}"
       end
