29e10ecd9a479eedb3347049716099c4800d90fa
Alex Zotov <alexandr.zotov@emory.edu>
2024-07-02 11:57:05 -0500

    moved controller

---
+++ app/controllers/purl_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+class PurlController < ApplicationController
+  def redirect_to_original
+    solr_object = Hyrax.custom_queries.find_by_alternate_id(alternate_ids: params[:alternate_ids])
+    if solr_object.has_model.present? && solr_object.has_model.first == 'Publication'
+      object_url = "#{request.base_url}/concern/publications/#{solr_object.id}"
+    elsif solr_object.respond_to?(:collection_type_gid) && solr_object.collection_type_gid.present?
+      object_url = "#{request.base_url}/collections/#{solr_object.id}"
+    else
+      render plain: 'Item not found', status: :not_found
+    end
+    redirect_to object_url
+  end
+end
