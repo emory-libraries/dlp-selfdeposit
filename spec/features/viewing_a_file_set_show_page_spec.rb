1bcb6aa676ea418e1d5dd2a009bb6a932395215f
bwatson78 <brad.watson.orlando@gmail.com>
2024-07-08 16:35:48 -0500

    sets up feature test.

---
+++ spec/features/viewing_a_file_set_show_page_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+require 'rails_helper'
+require 'hyrax/specs/shared_specs/factories/administrative_sets'
+require 'hyrax/specs/shared_specs/factories/permission_templates'
+require 'hyrax/specs/shared_specs/factories/workflows'
+include Warden::Test::Helpers
+
+RSpec.describe "viewing a FileSet's show page", :clean_repo, :perform_enqueued, type: :feature do
+  let!(:user) { FactoryBot.create(:user) }
+  let(:admin_set) { FactoryBot.valkyrie_create(:hyrax_admin_set) }
+  let(:permission_template) { FactoryBot.create(:permission_template, source_id: admin_set.id) }
+  let!(:workflow) { FactoryBot.create(:workflow, allows_access_grant: true, active: true, permission_template_id: permission_template.id) }
+  let!(:file_set) { FactoryBot.valkyrie_create(:hyrax_file_set, :with_files, title: ['Test File Set'], depositor: user.user_key, read_groups: ['public'], edit_users: [user]) }
+  let!(:publication) { FactoryBot.valkyrie_create(:publication, admin_set_id: admin_set.id, depositor: user.user_key, members: [file_set]) }
+
+  before do
+    publication
+    pulled_file_set = Hyrax.query_service.find_by(id: publication.member_ids.first)
+    Hyrax.index_adapter.save(resource: pulled_file_set)
+    allow_any_instance_of(SolrDocument).to receive(:creating_application_name).and_return(['ImageMagick'])
+    allow_any_instance_of(SolrDocument).to receive(:creating_os).and_return(['MacOSX Sapphire'])
+    login_as user
+    visit hyrax_file_set_path(file_set)
+  end
+
+  it 'contains the custom characterization elements' do
+    expect(page).to have_content('File Path: ')
+    expect(page).to have_content('image.png') # once in File Path, once in Preservation Events
+    expect(page).to have_content('Creating Application Name: ImageMagick')
+    expect(page).to have_content('Creating Os: MacOSX Sapphire')
+    expect(page).to have_content('Persistent Unique Identification: fmt')
+    expect(page).to have_content('Original Checksum: ')
+    expect(page).to have_content('urn:sha1:3ed1da08d5d0a400612216bc0134780d7495b54e')
+    expect(page).to have_content('urn:md5:354c7b6da70b120e897c4df08e74e6ac')
+    expect(page).to have_content('urn:sha256:3f97a01efdd0ea847a24aecad6f4bfa8640838d393e35cc553408908ace0928e')
+  end
+
+  it 'contains a table of Preservation Events' do
+    expect(page).to have_css('h2.fs-preservation-events-header', text: 'Preservation Events')
+    # NOTE: the below test is randomly failing, most likely to the page loading before PreservationEvents are fully persisted.
+    expect(find_all('table#fs-preservation-event-table tbody tr')).to be_present # expect at least one body row
+  end
+end
