30308f87d28c8915a6c5395ba4d0b3f41d0707c3
Alex Zotov <alexandr.zotov@emory.edu>
2024-07-18 14:13:53 -0500

    creates custom collection type, and default collection

---
+++ lib/tasks/admin_set_collection.rake
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+namespace :selfdeposit do
+  namespace :publications do
+    desc "Automate setup of OE Admin Set, Collection Type and Collection"
+    task setup_admin_set_collection: :environment do
+      ENV.fetch('OPENEMORY_WORKFLOW_ADMIN_SET_ID', Hyrax::AdminSetCreateService.find_or_create_default_admin_set.id.to_s)
+      col_type = CollectionType.find_or_create_library_collection_type
+      make_default_collection(col_type)
+    end
+  end
+end
+
+def make_default_collection(col_type)
+  col = Collection.new
+  col.title = ["OpenEmory"]
+  col.creator = ['Emory University']
+  col.holding_repository = ['Emory University. Library']
+  col.institution = ['Emory University']
+  col.description = ['OpenEmory preserves scholarly publications from Emory faculty, graduate, and undergraduate researchers and makes them freely accessible, increasing the visibility of this research and furthering the intellectual community at Emory University.']
+  col.collection_type_gid = col_type.to_global_id.to_s
+  col.visibility = "open"
+  change_set = Hyrax::ChangeSet.for(col)
+  tx = Hyrax::Transactions::CollectionCreate.new
+  tx.with_step_args('change_set.set_noid_id' => {}).call(change_set)
+end
---
+++ spec/models/collection_type_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# Goal: Easily create Collection objects that follow the template laid out by this
+# CollectionType created by Emily:
+# => #<Hyrax::CollectionType id: 3, title: "Library Collection",
+# description: "Library staff-curated collections", machine_id: "library_collection",
+# nestable: true, discoverable: true, sharable: true, allow_multiple_membership: false,
+# require_membership: false, assigns_workflow: false, assigns_visibility: false,
+# share_applies_to_new_works: true, brandable: true, badge_color: "#663333">
+
+RSpec.describe CollectionType, :clean, type: :model do
+  let(:collection_type) { described_class.new }
+
+  it "we can find or create it easily and it won't proliferate" do
+    col_t = described_class.count
+    described_class.find_or_create_library_collection_type
+    expect(described_class.count).to eq 1 + col_t
+    described_class.find_or_create_library_collection_type
+    expect(described_class.count).to eq 1 + col_t
+  end
+
+  it 'only allows users with the admin role to manage' do
+    # It does not have Registered Users group in the Creators role
+    ct = described_class.find_or_create_library_collection_type
+    collection_type_participants = Hyrax::CollectionTypeParticipant.where(hyrax_collection_type_id: ct.id)
+    expect(collection_type_participants.size).to eq 1
+    expect(collection_type_participants.first.agent_id).to eq "admin"
+    expect(collection_type_participants.first.access).to eq "manage"
+  end
+
+  it "is named Library Collection" do
+    expect(collection_type.title).to eq "Library Collection"
+  end
+
+  it 'has a description' do
+    expect(collection_type.description).to eq "Library staff curated collections"
+  end
+
+  it 'has a machine_id' do
+    expect(collection_type.machine_id).to eq "library_collection"
+  end
+
+  it 'has a specific badge color' do
+    expect(collection_type.badge_color).to eq "#663333"
+  end
+
+  it "has nesting enabled" do
+    expect(collection_type.nestable?).to eq true
+  end
+
+  it "is brandable" do
+    expect(collection_type.brandable?).to eq true
+  end
+
+  it "is discoverable" do
+    expect(collection_type.discoverable?).to eq true
+  end
+
+  it "is sharable" do
+    expect(collection_type.sharable).to eq true
+  end
+
+  it "has share_applies_to_new_works enabled" do
+    expect(collection_type.share_applies_to_new_works?).to eq true
+  end
+
+  it "has multiple membership disabled" do
+    expect(collection_type.allow_multiple_membership?).to eq false
+  end
+
+  it "has default settings for everything else" do
+    expect(collection_type.require_membership).to eq false
+    expect(collection_type.assigns_workflow?).to eq false
+    expect(collection_type.assigns_visibility?).to eq false
+  end
+
+  describe ".any_nestable?" do
+    context "when there is a nestable collection type" do
+      it 'returns true' do
+        collection_type.save
+        expect(described_class.any_nestable?).to be true
+      end
+    end
+  end
+end
