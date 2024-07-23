251f75ddde130af10143cbc2d8d57850114bd193
bwatson78 <brad.watson.orlando@gmail.com>
2024-07-23 09:05:16 -0500

    Adds local processing to parse fields.

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -4,5 +4,21 @@ module Bulkrax::HasLocalProcessing
   # This method is called during build_metadata
   # add any special processing here, for example to reset a metadata property
   # to add a custom property from outside of the import data
-  def add_local; end
+  def add_local
+    # data_classification
+    parsed_metadata['data_classification'] = parsed_metadata['data_classification'].blank? ? 'Public' : parsed_metadata['data_classification'].strip
+
+    # publisher
+    parsed_metadata['publisher'] = parsed_metadata['publisher'].blank? ? 'Emory University Libraries' : parsed_metadata['publisher'].strip
+
+    process_emory_content_type
+  end
+
+  def process_emory_content_type
+    authority = Qa::Authorities::Local.subauthority_for('emory_content_types')
+    default_type = "http://id.loc.gov/vocabulary/resourceTypes/txt"
+    pulled_type = authority.all.find { |v| v['label'] == parsed_metadata['emory_content_type']&.strip&.titleize }
+
+    parsed_metadata['emory_content_type'] = pulled_type.fetch('id', default_type)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_21_192828) do
+ActiveRecord::Schema.define(version: 2024_07_22_184704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -648,6 +648,7 @@ ActiveRecord::Schema.define(version: 2024_06_21_192828) do
     t.datetime "updated_at", null: false
     t.string "fileset_name"
     t.string "fileset_use"
+    t.string "desired_visibility"
     t.index ["file_set_uri"], name: "index_uploaded_files_on_file_set_uri"
     t.index ["user_id"], name: "index_uploaded_files_on_user_id"
   end
