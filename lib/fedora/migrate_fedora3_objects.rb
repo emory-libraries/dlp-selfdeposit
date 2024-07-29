3cd692d7195598e84ecc89abdf334e4c1ea92f23
bwatson78 <brad.watson.orlando@gmail.com>
2024-07-29 15:51:25 -0500

    Extracts the migration class to make writing RSpec easier.

--- lib/fedora/migrate_fedora3_objects.rb
+++ lib/fedora/migrate_fedora3_objects.rb
@@ -1,49 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-require 'fileutils'
-require 'nokogiri'
-require 'open-uri'
-require 'csv'
-require_relative 'fedora_three_objects_migration_methods'
-
-class MigrateFedoraThreeObjects
-  ::ALLOWED_TYPES = {
-    'application/pdf': 'pdf',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
-    'application/msword': 'doc',
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
-    'application/vnd.ms-powerpoint': 'ppt',
-    'image/jpeg': 'jpeg',
-    'image/tiff': 'tiff',
-    'image/png': 'png',
-    'application/vnd.ms-excel': 'xls',
-    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx'
-  }.freeze
-
-  include FedoraThreeObjectsMigrationMethods
-
-  def initialize(pids:, fedora_three_path:, fedora_username:, fedora_password:)
-    @pids = pids
-    @fedora_three_path = fedora_three_path
-    @fedora_username = fedora_username
-    @fedora_password = fedora_password
-    @pids_with_no_binaries = []
-    @pids_with_filenames = {}
-    @date_time_started = DateTime.now.strftime('%Y%m%dT%H%M')
-  end
-
-  def run
-    pid_arr = @pids.include?('.csv') ? pull_pids_csv : @pids.split(',')
-
-    pid_arr.each do |pid|
-      @pid = pid
-      @pid_xml = pull_pid_xml
-      copy_files_to_folder
-    end
-
-    file_end_reports
-  end
-end
+require_relative 'migrate_fedora_three_objects'
 
 if ARGV.length != 4
   puts "Exiting -- This script must be in the following format:"
