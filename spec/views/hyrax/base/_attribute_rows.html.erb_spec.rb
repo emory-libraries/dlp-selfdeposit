b827c5cfee67ed48046b5ef28dadf9ad89b3c304
C Barton <cbarto8@emory.edu>
2024-08-30 17:13:47 -0400

    Updates test.

--- spec/views/hyrax/base/_attribute_rows.html.erb_spec.rb
+++ spec/views/hyrax/base/_attribute_rows.html.erb_spec.rb
@@ -64,6 +64,27 @@ RSpec.describe "hyrax/base/_attribute_rows.html.erb", type: :view do
     allow(view).to receive(:current_user).and_return(user)
   end
 
+  context "user is not logged in or not admin" do
+
+    it "shows a publication's labels" do
+      ["Persistent URL", "Last modified", "Type of Material", "Authors", "Language", "Date", "Publisher", "Publication Version",
+       "License", "Final Published Version (URL)", "Title of Journal or Parent Work", "Conference or Event Name", "ISSN", "ISBN", "Series Title",
+       "Edition", "Volume", "Issue", "Start Page", "End Page", "Place of Publication or Presentation", "Sponsor", "Grant/Funding Agency",
+       "Grant/Funding Information", "Supplemental Material (URL)", "Abstract", "Author Notes", "Keywords", "Subject - Topics", "Research Categories"].each do |label|
+        expect(page).to have_selector 'dt', text: label
+      end
+    end
+
+    it "shows a publication's values" do
+      ["904dncjsz6-emory", "05/12/2024", "Article", "Tom Collins, Gin University", "English", "2004", "Simon & Schusters", "Final Publisher PDF",
+       "Creative Commons Attribution-ShareAlike 4.0 International", "1020", "Parent title",
+       "Samvera Connect", "fqefeefe2fed", "dvqegr3gvfw", "Series Title", "1", "4", "3", "1", "134", "Boston", "NEA", "Rockfeller", "Granted",
+       "A Dataset", "an abstraction", "A note from the author.", "keywordbooks", "Mathematics", "Asian studies"].each do |value|
+        expect(page).to have_selector 'dd', text: value
+      end
+    end
+  end
+
   context "user is admin" do
     before { allow(user).to receive(:admin?).and_return(true) }
 
