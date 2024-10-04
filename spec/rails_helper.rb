e7ebb1a6b39a77977ccc29bc4978a22cadb504a2
bwatson78 <brad.watson.orlando@gmail.com>
2024-10-04 12:48:01 -0500

    Reworks the spec and rails helpers to make rspec work consistently.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,30 @@
 # frozen_string_literal: true
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'devise'
-require 'devise_saml_authenticatable'
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../config/environment', __dir__)
-
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'devise'
+require 'devise_saml_authenticatable'
+require 'hyrax/specs/shared_specs/factories/strategies/json_strategy'
+require 'hyrax/specs/shared_specs/factories/strategies/valkyrie_resource'
+require 'hyrax/specs/shared_specs/factories/users'
+require 'hyrax/specs/capybara'
+require 'rspec/active_model/mocks'
+require 'database_cleaner'
+
 require 'valkyrie'
 Valkyrie::MetadataAdapter.register(Valkyrie::Persistence::Memory::MetadataAdapter.new, :test_adapter)
 Valkyrie::StorageAdapter.register(Valkyrie::Storage::Memory.new, :memory)
 
-require 'hyrax/specs/capybara'
+require 'factory_bot'
+FactoryBot.register_strategy(:valkyrie_create, ValkyrieCreateStrategy)
+FactoryBot.register_strategy(:json, JsonStrategy)
+FactoryBot.find_definitions
 
 query_registration_target =
   Valkyrie::MetadataAdapter.find(:test_adapter).query_service.custom_queries
@@ -41,7 +51,6 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-
 RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec', 'fixtures').to_s
   config.use_transactional_fixtures = true
@@ -53,4 +62,51 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  # The following behaviors are copied from Hyrax v5.0.1's spec_helper.rb
+  config.before :suite do
+    DatabaseCleaner.clean_with(:truncation)
+    User.group_service = TestHydraGroupService.new
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+    User.group_service.clear
+  end
+
+  config.before do |example|
+    if example.metadata[:type] == :feature && Capybara.current_driver != :rack_test
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.start
+    end
+
+    # using :workflow is preferable to :clean_repo, use the former if possible
+    # It's important that this comes after DatabaseCleaner.start
+    ensure_deposit_available_for(user) if example.metadata[:workflow] && defined?(user)
+  end
+
+  config.prepend_before(:example, :storage_adapter) do |example|
+    adapter_name = example.metadata[:storage_adapter]
+
+    allow(Hyrax)
+      .to receive(:storage_adapter)
+      .and_return(Valkyrie::StorageAdapter.find(adapter_name))
+  end
+
+  config.prepend_before(:example, :valkyrie_adapter) do |example|
+    adapter_name = example.metadata[:valkyrie_adapter]
+
+    if adapter_name == :wings_adapter
+      skip("Don't test Wings when it is disabled") if Hyrax.config.disable_wings
+    else
+      allow(Hyrax.config).to receive(:disable_wings).and_return(true)
+      hide_const("Wings") # disable_wings=true removes the Wings constant
+    end
+
+    allow(Hyrax)
+      .to receive(:metadata_adapter)
+      .and_return(Valkyrie::MetadataAdapter.find(adapter_name))
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,6 @@
 # frozen_string_literal: true
-require 'factory_bot'
-require 'hyrax/specs/shared_specs/factories/strategies/json_strategy'
-require 'hyrax/specs/shared_specs/factories/strategies/valkyrie_resource'
-require 'hyrax/specs/shared_specs/factories/users'
 require 'coveralls'
-require 'rspec/active_model/mocks'
-require 'database_cleaner'
-
-ENV['RAILS_ENV'] = 'test'
-
-unless ENV['CI']
-  ENV['DATABASE_URL'] = ENV['DATABASE_TEST_URL'] if ENV['DATABASE_TEST_URL']
-  db_config = ActiveRecord::Base.configurations[ENV['RAILS_ENV']]
-
-  ActiveRecord::Tasks::DatabaseTasks.create(db_config)
-  ActiveRecord::Migrator.migrations_paths = [Pathname.new(ENV['RAILS_ROOT']).join('db', 'migrate').to_s]
-  ActiveRecord::Tasks::DatabaseTasks.migrate
-  ActiveRecord::Base.descendants.each(&:reset_column_information)
-  ActiveRecord::Migration.maintain_test_schema!
-end
-
 Coveralls.wear!('rails')
-
-FactoryBot.register_strategy(:valkyrie_create, ValkyrieCreateStrategy)
-FactoryBot.register_strategy(:json, JsonStrategy)
-FactoryBot.find_definitions
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -42,51 +18,4 @@ RSpec.configure do |config|
   config.profile_examples = 10
   config.order = :random
   Kernel.srand config.seed
-
-  # The following behaviors are copied from Hyrax v5.0.1's spec_helper.rb
-  config.before :suite do
-    DatabaseCleaner.clean_with(:truncation)
-    User.group_service = TestHydraGroupService.new
-  end
-
-  config.after do
-    DatabaseCleaner.clean
-    User.group_service.clear
-  end
-
-  config.before do |example|
-    if example.metadata[:type] == :feature && Capybara.current_driver != :rack_test
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.start
-    end
-
-    # using :workflow is preferable to :clean_repo, use the former if possible
-    # It's important that this comes after DatabaseCleaner.start
-    ensure_deposit_available_for(user) if example.metadata[:workflow] && defined?(user)
-  end
-
-  config.prepend_before(:example, :storage_adapter) do |example|
-    adapter_name = example.metadata[:storage_adapter]
-
-    allow(Hyrax)
-      .to receive(:storage_adapter)
-      .and_return(Valkyrie::StorageAdapter.find(adapter_name))
-  end
-
-  config.prepend_before(:example, :valkyrie_adapter) do |example|
-    adapter_name = example.metadata[:valkyrie_adapter]
-
-    if adapter_name == :wings_adapter
-      skip("Don't test Wings when it is disabled") if Hyrax.config.disable_wings
-    else
-      allow(Hyrax.config).to receive(:disable_wings).and_return(true)
-      hide_const("Wings") # disable_wings=true removes the Wings constant
-    end
-
-    allow(Hyrax)
-      .to receive(:metadata_adapter)
-      .and_return(Valkyrie::MetadataAdapter.find(adapter_name))
-  end
 end
