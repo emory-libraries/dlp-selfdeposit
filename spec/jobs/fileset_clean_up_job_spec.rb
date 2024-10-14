4da17254feb8a747b0ee0b9a6bec372477edd5f6
Alex Zotov <alexandr.zotov@emory.edu>
2024-10-14 07:20:11 -0500

    adds fileset cleanup job and cleanup task (#563)
    * adds fileset cleanup job and cleanup task
    
    * fixes find_by methods
    
    * fixes job querying and spec
    
    * creates dir if it does not exist

---
+++ spec/jobs/fileset_clean_up_job_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+# frozen_string_literal: true
+require 'rails_helper'
+require 'hyrax/specs/shared_specs/factories/administrative_sets'
+require 'hyrax/specs/shared_specs/factories/permission_templates'
+require 'hyrax/specs/shared_specs/factories/workflows'
+
+RSpec.describe FilesetCleanUpJob, :clean do
+  let(:csv_path) { Rails.root.join('tmp', 'emory', 'index_file_set_results.csv') }
+  let(:csv) { CSV.read(csv_path) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin_set) { FactoryBot.valkyrie_create(:hyrax_admin_set) }
+  let(:permission_template) { FactoryBot.create(:permission_template, source_id: admin_set.id) }
+  let!(:workflow) { FactoryBot.create(:workflow, allows_access_grant: true, active: true, permission_template_id: permission_template.id) }
+  let(:query_service) { Hyrax.query_service }
+
+  before do
+    allow(ValkyrieCharacterizationJob).to receive(:perform_later)
+  end
+
+  after do
+    File.delete(csv_path) if File.exist?(csv_path)
+  end
+
+  shared_examples 'file set characterization' do
+    it 'queues characterization job and logs to CSV' do
+      expect(ValkyrieCharacterizationJob).to have_received(:perform_later).with(file_set.file_ids.first).at_least(:once)
+      expect(csv).to include([file_set.id.to_s, "Fileset not characterized", "Queued"])
+    end
+  end
+
+  context 'when no file_set_ids are provided' do
+    let!(:file_set1) { FactoryBot.valkyrie_create(:hyrax_file_set, :with_files, title: ['Test File Set 1'], depositor: user.user_key, read_groups: ['public'], edit_users: [user]) }
+    let!(:file_set2) { FactoryBot.valkyrie_create(:hyrax_file_set, :with_files, title: ['Test File Set 2'], depositor: user.user_key, read_groups: ['public'], edit_users: [user]) }
+
+    before do
+      allow(query_service).to receive(:find_all_of_model).with(model: FileSet).and_return([file_set1, file_set2])
+      described_class.perform_now
+    end
+
+    it 'processes all file sets' do
+      expect(query_service).to have_received(:find_all_of_model).with(model: FileSet)
+      expect(ValkyrieCharacterizationJob).to have_received(:perform_later).at_least(:once)
+    end
+
+    it_behaves_like 'file set characterization' do
+      let(:file_set) { file_set1 }
+    end
+
+    it_behaves_like 'file set characterization' do
+      let(:file_set) { file_set2 }
+    end
+  end
+
+  context 'when specific file_set_ids are provided' do
+    let!(:file_set1) { FactoryBot.valkyrie_create(:hyrax_file_set, :with_files, title: ['Test File Set 1'], depositor: user.user_key, read_groups: ['public'], edit_users: [user]) }
+    let!(:file_set2) { FactoryBot.valkyrie_create(:hyrax_file_set, :with_files, title: ['Test File Set 2'], depositor: user.user_key, read_groups: ['public'], edit_users: [user]) }
+
+    before do
+      allow(query_service).to receive(:find_by).and_return(file_set1, file_set2)
+      described_class.perform_now(file_set1.id, file_set2.id)
+    end
+
+    it 'processes only the specified file sets' do
+      expect(query_service).to have_received(:find_by).with(id: file_set1.id)
+      expect(query_service).to have_received(:find_by).with(id: file_set2.id)
+      expect(ValkyrieCharacterizationJob).to have_received(:perform_later).at_least(:once)
+    end
+
+    it_behaves_like 'file set characterization' do
+      let(:file_set) { file_set1 }
+    end
+
+    it_behaves_like 'file set characterization' do
+      let(:file_set) { file_set2 }
+    end
+  end
+end
