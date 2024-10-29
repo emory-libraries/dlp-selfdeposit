8a75d99b0b50e853a45be62365f3346982e10319
Brad Watson <brad.watson.orlando@gmail.com>
2024-10-29 11:06:15 -0500

    Corrects the fileset rake task. (#603)

--- app/jobs/fileset_clean_up_job.rb
+++ app/jobs/fileset_clean_up_job.rb
@@ -3,15 +3,15 @@
 class FilesetCleanUpJob < Hyrax::ApplicationJob
   def perform(*file_set_ids)
     csv_path = Rails.root.join('tmp', 'emory', 'index_file_set_results.csv')
-    FileUtils.mkdir_p(File.dirname(csv_path))
+    ::FileUtils.mkdir_p(File.dirname(csv_path))
     CSV.open(csv_path, "w") do |csv|
       if file_set_ids.present?
         file_set_ids.flatten.each do |id|
-          file_set = Hyrax.query_service.find_by(id:)
+          file_set = ::Hyrax.query_service.find_by(id:)
           process_fileset(file_set, csv)
         end
       else
-        Hyrax.query_service.find_all_of_model(model: ::FileSet).each do |file_set|
+        ::Hyrax.query_service.find_all_of_model(model: ::FileSet).each do |file_set|
           process_fileset(file_set, csv)
         end
       end
@@ -25,7 +25,7 @@ class FilesetCleanUpJob < Hyrax::ApplicationJob
   end
 
   def characterize_file_set(file_set, csv)
-    ValkyrieCharacterizationJob.perform_later(file_set.original_file_id)
+    ::ValkyrieCharacterizationJob.perform_later(file_set.original_file_id)
     csv << [file_set.id, "Fileset not characterized", "Queued"]
   end
 end
