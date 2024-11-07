942650d65e131e529895c3751f529be61e446018
Alex Zotov <alexandr.zotov@emory.edu>
2024-11-07 10:27:07 -0600

    fixes rubocop errors

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def saml
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in @user
-     redirect_to request.env["omniauth.origin"] || hyrax.dashboard_path
+      redirect_to request.env["omniauth.origin"] || hyrax.dashboard_path
       set_flash_message(:notice, :success, kind: "SAML")
     else
       redirect_to root_path
@@ -15,4 +17,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path
   end
-end 
+end
