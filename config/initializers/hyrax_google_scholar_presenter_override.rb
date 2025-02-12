51520b6ddc502a9795c8591c225fee870f546f75
bwatson78 <brad.watson.orlando@gmail.com>
2025-02-12 15:31:38 -0600

    Customizes built in Google Scholar behavior.

---
+++ config/initializers/hyrax_google_scholar_presenter_override.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+# [Hyrax-overwrite-v5.0.1] - Adds or overrides value pull methods.
+
+Rails.application.config.to_prepare do
+  Hyrax::GoogleScholarPresenter.class_eval do
+    EMORY_METHOD_ASSIGNMENTS = [
+      ['publication_date', :date_issued_year],
+      ['journal_title', :parent_title],
+      ['conference_title', :conference_name],
+      ['issn', :issn],
+      ['isbn', :isbn],
+      ['volume', :volume],
+      ['issue', :issue],
+      ['firstpage', :page_range_start],
+      ['lastpage', :page_range_end]
+    ].freeze
+
+    EMORY_METHOD_ASSIGNMENTS.each do |method_name, solr_field|
+      define_method method_name do
+        object.try(solr_field)
+      end
+    end
+  end
+end
