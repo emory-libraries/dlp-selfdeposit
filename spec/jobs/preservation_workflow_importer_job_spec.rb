32c23d492a938a83cd4241f3a6e4250a751c9381
Alex Zotov <alexandr.zotov@emory.edu>
2025-03-03 09:59:36 -0600

    fixes importer tests

--- spec/jobs/preservation_workflow_importer_job_spec.rb
+++ spec/jobs/preservation_workflow_importer_job_spec.rb
@@ -2,22 +2,40 @@
 require 'rails_helper'
 
 RSpec.describe PreservationWorkflowImporterJob, :clean do
-  let(:persister) { Hyrax.persister }
-  let(:generic_work) do
-    Hyrax.custom_queries.find_publication_by_deduplication_key(deduplication_key: 'MSS1218_B071_I205')
+  let(:adapter)       { Valkyrie::MetadataAdapter.find(:test_adapter) }
+  let(:persister)     { adapter.persister }
+  let(:query_service) { adapter.query_service }
+  
+  let(:publication) do
+    publication = Publication.new(
+      data_classification: 'Public',
+      emory_content_type: 'Manuscript',
+      holding_repository: 'Emory Libraries',
+      creator: 'Dr. Seuss',
+      rights_statement: 'http://rightsstatements.org/vocab/NoC-NC/1.0/',
+      deduplication_key: 'MSS1218_B071_I205',
+      title: 'My Publication'
+    )
+    persister.save(resource: publication)
   end
+
   let(:csv) { fixture_path + '/preservation_workflows.csv' }
 
   before do
-    work = GenericWork.new(
-      title: ['Example title'],
-      deduplication_key: 'MSS1218_B071_I205'
-    )
-    persister.save(resource: work)
+    allow(Hyrax).to receive(:query_service).and_return(query_service)
+    allow(Hyrax).to receive(:persister).and_return(persister)
+    Hyrax.index_adapter.save(resource: publication)
+  end
+
+  after do
+    Hyrax.index_adapter.wipe!
   end
 
   it 'processes preservation workflow metadata' do
     described_class.perform_now(csv)
-    expect(generic_work.preservation_workflow.count).to eq 4
+    
+    queried_publication = query_service.find_by(id: publication.id)
+    expect(queried_publication.preservation_workflows.count).to eq 4
+    expect(queried_publication.preservation_workflows.pluck(:workflow_type)).to match_array PreservationWorkflowImporter.workflow_types
   end
 end
