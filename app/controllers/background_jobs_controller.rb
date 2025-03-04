0e60d29cc551bab1868f46e5e6b98b2b253fb5b8
Alex Zotov <alexandr.zotov@emory.edu>
2025-03-04 12:56:55 -0600

    Rake dashboard (#810)
    * implements rake dashboard
    
    * add importers, and jobs
    
    * adds tests for rake dashboard
    
    * fixes rubocop errors
    
    * changes name of the job
    
    * fixes tests
    
    * fixes auth for jobs
    
    * rubocop issues
    
    * fixes importer tests
    
    * fixes importer tests

---
+++ app/controllers/background_jobs_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+class BackgroundJobsController < ApplicationController
+  with_themed_layout 'dashboard'
+  before_action :authenticate_user!
+
+  def new; end
+
+  def create
+    process_non_preprocessor_jobs
+  end
+
+  private
+
+  def process_non_preprocessor_jobs
+    if params[:jobs] == 'preservation'
+      generic_csv_action(params[:preservation_csv], PreservationWorkflowImporterJob)
+      redirect_to new_background_job_path, notice: "Preservation Workflow Importer Job has started successfully."
+    else
+      reindex_objects_action
+      redirect_to new_background_job_path, notice: "Reindex Files Job has started successfully."
+    end
+  end
+
+  def reindex_objects_action
+    CSV.foreach(params[:reindex_csv].path, headers: true) do |row|
+      r = row.to_h
+      ReindexObjectJob.perform_later(r['id'])
+    end
+  end
+
+  def generic_csv_action(csv_param, job_class)
+    name = csv_param.original_filename
+    path = Rails.root.join('tmp', name)
+    File.open(path, "w+") { |f| f.write(csv_param.read) }
+    job_class.perform_later(path.to_s)
+  end
+end
