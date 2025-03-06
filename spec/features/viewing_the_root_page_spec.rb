dd422a6bdece9b9a6d9a98b75d1117e7fe069be7
bwatson78 <brad.watson.orlando@gmail.com>
2025-03-06 11:24:13 -0600

    Adds Total works deposited to homepage.

--- spec/features/viewing_the_root_page_spec.rb
+++ spec/features/viewing_the_root_page_spec.rb
@@ -1,11 +1,39 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require 'hyrax/specs/shared_specs/factories/administrative_sets'
 include Warden::Test::Helpers
 
-RSpec.describe 'viewing the root page' do
+RSpec.describe 'viewing the root page', :clean_repo, type: :feature do
   it 'has the welcome text' do
     visit root_path
 
     expect(page).to have_content('Emory Libraries')
+  end
+
+  context 'statistics' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:admin_set) { FactoryBot.valkyrie_create(:hyrax_admin_set) }
+    let(:publication) do
+      FactoryBot.valkyrie_create(:publication,
+                                 :public,
+                                 admin_set_id: admin_set.id,
+                                 depositor: user.user_key,
+                                 title: ['Al Pacino: A Life'],
+                                 creator: ['Pacino, Al, Emory University'],
+                                 date_issued_year: '1975',
+                                 parent_title: 'Father',
+                                 content_genre: 'Book',
+                                 emory_content_type: 'http://id.loc.gov/vocabulary/resourceTypes/txt',
+                                 publisher_version: 'Author Accepted Manuscript (After Peer Review)',
+                                 keyword: ['Godfather', 'Scent of a Woman', ""])
+    end
+
+    it 'reports 1 work deposited' do
+      Hyrax.index_adapter.wipe!
+      publication
+      visit root_path
+
+      expect(page).to have_css('#total-works-num', text: '1')
+    end
   end
 end
