c9e46e4e2a2173c50987d55bfc309f2fcaba1bc0
Alex Zotov <alexandr.zotov@emory.edu>
2025-03-12 14:37:24 -0500

    removes omniauth custom controller for now

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
-class OmniauthController < Devise::SessionsController
-  def new
-    if Rails.env.production?
-      redirect_to user_saml_omniauth_authorize_path
-    else
-      super
-    end
-  end
-end
+# class OmniauthController < Devise::SessionsController
+#   def new
+#     if Rails.env.production?
+#       redirect_to user_saml_omniauth_authorize_path
+#     else
+#       super
+#     end
+#   end
+# end
--- spec/controllers/omniauth_controller_spec.rb
+++ spec/controllers/omniauth_controller_spec.rb
@@ -1,59 +1,68 @@
 # frozen_string_literal: true
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe OmniauthController do
-  describe '#new' do
-    before do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      @request.env['warden'] = double(Warden::Proxy)
-      allow(@request.env['warden']).to receive(:authenticate?).and_return(false)
-      allow(@request.env['warden']).to receive(:authenticate!).and_return(false)
-    end
+# RSpec.describe OmniauthController, type: :controller do
+#   # Set the controller to use Devise's layout
+#   routes { Devise::Engine.routes }
 
-    context 'when in production environment' do
-      before do
-        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production'))
-      end
+#   describe '#new' do
+#     before do
+#       @request.env['devise.mapping'] = Devise.mappings[:user]
+#       @request.env['warden'] = double(Warden::Proxy)
+#       allow(@request.env['warden']).to receive(:authenticate?).and_return(false)
+#       allow(@request.env['warden']).to receive(:authenticate!).and_return(false)
+#     end
 
-      it 'redirects to shibboleth login' do
-        get :new
-        expect(response).to redirect_to(user_saml_omniauth_authorize_path)
-      end
-    end
+#     context 'when not in production environment' do
+#       before do
+#         allow(Rails.env).to receive(:production?).and_return(false)
+#       end
 
-    context 'when not in production environment' do
-      before do
-        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
-      end
+#       it 'calls the superclass implementation' do
+#         expect_any_instance_of(Devise::SessionsController).to receive(:new)
+#         get :new
+#       end
+#     end
 
-      it 'renders the default devise login page' do
-        get :new
-        expect(response).to be_successful
-        expect(response).to render_template('devise/sessions/new')
-      end
-    end
-  end
+#     context 'when in production environment' do
+#       before do
+#         allow(Rails.env).to receive(:production?).and_return(true)
+#       end
 
-  describe 'routing' do
-    it 'routes GET /sign_in to omniauth#new' do
-      expect(get: '/sign_in').to route_to(
-        controller: 'omniauth',
-        action: 'new'
-      )
-    end
+#       it 'redirects to saml login' do
+#         get :new
+#         expect(response).to redirect_to('/auth/saml')
+#       end
+#     end
+#   end
 
-    it 'routes POST /sign_in to users/omniauth_callbacks#saml' do
-      expect(post: '/sign_in').to route_to(
-        controller: 'users/omniauth_callbacks',
-        action: 'saml'
-      )
-    end
+#   describe 'routing' do
+#     it 'routes POST /auth/saml/callback to omniauth_callbacks#saml' do
+#       expect(post: '/auth/saml/callback').to route_to(
+#         controller: 'omniauth_callbacks',
+#         action: 'saml'
+#       )
+#     end
 
-    it 'routes GET /sign_out to devise/sessions#destroy' do
-      expect(get: '/sign_out').to route_to(
-        controller: 'devise/sessions',
-        action: 'destroy'
-      )
-    end
-  end
-end
+#     it 'routes POST /auth/saml to omniauth_callbacks#passthru' do
+#       expect(post: '/auth/saml').to route_to(
+#         controller: 'omniauth_callbacks',
+#         action: 'passthru'
+#       )
+#     end
+
+#     it 'routes GET /auth/failure to users/omniauth_callbacks#failure' do
+#       expect(get: '/auth/failure').to route_to(
+#         controller: 'users/omniauth_callbacks',
+#         action: 'failure'
+#       )
+#     end
+
+#     it 'routes GET /sign_out to devise/sessions#destroy' do
+#       expect(get: '/sign_out').to route_to(
+#         controller: 'devise/sessions',
+#         action: 'destroy'
+#       )
+#     end
+#   end
+# end
