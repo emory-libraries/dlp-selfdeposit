7e825532568f1b47f0b5e2cba1aebd72512efacd
bwatson78 <brad.watson.orlando@gmail.com>
2025-05-01 15:52:52 -0500

    corrects rubocop and adds gtag endpoint.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,15 +6,15 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-  policy.script_src  :self, 'https://emory.libwizard.com/form_loader.php?id=c1f0cb426fc77f8491d3b19eab369b9b&noheader=1'
-#   policy.style_src   :self, :https
+  #   policy.default_src :self, :https
+  #   policy.font_src    :self, :https, :data
+  #   policy.img_src     :self, :https, :data
+  #   policy.object_src  :none
+  policy.script_src :self, 'https://emory.libwizard.com/form_loader.php?id=c1f0cb426fc77f8491d3b19eab369b9b&noheader=1 https://www.googletagmanager.com/gtag/js?id=G-JJ43YGFK4G'
+  #   policy.style_src   :self, :https
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
+  #   # Specify URI for violation reports
+  #   # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
