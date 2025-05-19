ff4ec86ebbdda56da27756b51a01e650b51061cc
bwatson78 <brad.watson.orlando@gmail.com>
2025-05-19 11:34:00 -0500

    Installs Honeybadger.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -4,3 +4,4 @@ include Ec2Ipv4Retriever
 
 server find_ip_by_ec2_name(ec2_name: 'OE24-hyrax-prod') || ENV['PROD_SERVER_IP'], user: 'deploy', roles: [:web, :app, :db, :ubuntu]
 set :stage, :PROD
+set :honeybadger_env, "SelfDeposit-Prod"
--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -4,3 +4,4 @@ include Ec2Ipv4Retriever
 
 server find_ip_by_ec2_name(ec2_name: 'OE24-hyrax-test') || ENV['TEST_SERVER_IP'], user: 'deploy', roles: [:web, :app, :db, :ubuntu]
 set :stage, :TEST
+set :honeybadger_env, "SelfDeposit-Test"
