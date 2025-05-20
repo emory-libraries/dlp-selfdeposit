c3cd1ccb37e6396aec8fc816e47010f431c39500
Brad Watson <brad.watson.orlando@gmail.com>
2025-05-20 13:45:24 -0500

    Installs Honeybadger. (#1024)

--- config/deploy/arch.rb
+++ config/deploy/arch.rb
@@ -4,3 +4,4 @@ include Ec2Ipv4Retriever
 
 server find_ip_by_ec2_name(ec2_name: 'OE24-hyrax-arch') || ENV['ARCH_SERVER_IP'], user: 'deploy', roles: [:web, :app, :db, :ubuntu]
 set :stage, :ARCH
+set :honeybadger_env, "SelfDeposit-Arch"
