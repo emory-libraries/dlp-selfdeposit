f4cd8360951062d3336b7e36290df9e26b24c6b1
bwatson78 <brad.watson.orlando@gmail.com>
2025-05-21 10:23:02 -0500

    Swaps query to Hyrax::SolrService.

--- app/services/self_deposit/custom_queries/find_all_file_set_ids_associated_to_publications.rb
+++ app/services/self_deposit/custom_queries/find_all_file_set_ids_associated_to_publications.rb
@@ -6,38 +6,29 @@ module SelfDeposit
     #   Hyrax.custom_queries.find_all_file_set_ids_associated_to_publications
     #
     # @see https://github.com/samvera/valkyrie/wiki/Queries#custom-queries
-    class FindAllFileSetIdsAssociatedToPublications < SolrDocumentMultipleReturnQuery
+    class FindAllFileSetIdsAssociatedToPublications
       self.queries = [:find_all_file_set_ids_associated_to_publications]
 
       ##
-      # @return enumerator of Valkyrie Fedora objects
+      # @return Array[file set ids] or nil
       def find_all_file_set_ids_associated_to_publications
-        enum_for(:each)&.to_a&.flatten&.compact&.uniq
+        Hyrax::SolrService
+          &.query_result(query, fl: fields_selection, rows: 10_000_000)
+          &.[]('response')
+          &.[]('docs')
+          &.map { |doc| doc[fields_selection] }
+          &.flatten
+          &.uniq
       end
 
-      # Queries the Solr index for parent works of the provided resource
-      # For each Document, it yields the pulled Hyrax Work object
-      # @yield [Valkyrie::Resources]
-      def each
-        docs = Valkyrie::Persistence::Solr::Queries::DefaultPaginator.new
-        while docs.has_next?
-          docs = @connection.paginate(
-                   docs.next_page, docs.per_page, "select", params: { q: query, fq: filter_query, fl: fields_selection }
-                 )["response"]["docs"]
-          docs.each { |doc| doc["member_ids_ssim"].each { |id| yield id } }
-        end
-      end
-
-      # Solr query for for a Publication with a deduplication_key_tesi that matches the provided key
-      # @return [Hash]
+      # Solr query for for Publications with value(s) in the member_ids_ssim field
+      # @return String
       def query
-        "member_ids_ssim:*"
+        "member_ids_ssim:* && has_model_ssim:Publication"
       end
 
-      def filter_query
-        "has_model_ssim:Publication"
-      end
-
+      # Field(s) that the search results are restricted to
+      # @return String
       def fields_selection
         "member_ids_ssim"
       end
