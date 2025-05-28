93b7d171a977175860798152662649e96d167bc4
bwatson78 <brad.watson.orlando@gmail.com>
2025-05-28 10:45:36 -0500

    removes unnecessary actions.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,12 +3,4 @@ class ErrorsController < ApplicationController
   def not_found
     render status: :not_found
   end
-
-  # def unhandled_exception
-  #   render status: :internal_server_error
-  # end
-
-  # def unprocessable
-  #   render status: :unprocessable_entity
-  # end
 end
