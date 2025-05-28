9ed772667ef155b41660283cd1990b24d5c6f76a
bwatson78 <brad.watson.orlando@gmail.com>
2025-05-28 10:40:08 -0500

    tests request elements.

---
+++ spec/requests/errors_controller_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe ErrorsController, type: :request do
+  let(:request) { -> { get '/catalog/1' } }
+
+  it "loads the page when 404 error detected" do
+    rails_respond_without_detailed_exceptions do
+      request.call
+    end
+
+    expect(response.status).to eq 404
+    expect(response.body).not_to be_empty
+    expect(response.content_length).to be > 0
+    expect(response.content_type).to eq 'text/html; charset=utf-8'
+    expect(response).to render_template(:not_found)
+  end
+end
