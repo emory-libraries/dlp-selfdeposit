439e8fbd9616c4a1417e233f70ec1605e88d16ed
Brad Watson <brad.watson.orlando@gmail.com>
2025-06-02 14:38:09 -0500

    Switches FindAllAssociatedPreservationEventIds to Hyrax::SolrService. (#1035)
    * Switches FindAllAssociatedPreservationEventIds to Hyrax::SolrService.
    
    * fixes variable assignment.

--- app/services/self_deposit/custom_queries/find_all_associated_preservation_event_ids.rb
+++ app/services/self_deposit/custom_queries/find_all_associated_preservation_event_ids.rb
@@ -6,33 +6,49 @@ module SelfDeposit
     #   Hyrax.custom_queries.find_all_associated_preservation_event_ids
     #
     # @see https://github.com/samvera/valkyrie/wiki/Queries#custom-queries
-    class FindAllAssociatedPreservationEventIds < SolrDocumentMultipleReturnQuery
-      self.queries = [:find_all_associated_preservation_event_ids]
-
-      ##
-      # @returns [PreservationEvent#id]
-      def find_all_associated_preservation_event_ids
-        enum_for(:each)
+    class FindAllAssociatedPreservationEventIds
+      def self.queries
+        [:find_all_associated_preservation_event_ids]
       end
 
-      # Queries the Solr index for all objects associated to PreservationEvent objects.
-      # For each Document, it yields the list of pulled PreservationEvents' ids.
-      # @yield [PreservationEvent#id]
-      def each
-        docs = Valkyrie::Persistence::Solr::Queries::DefaultPaginator.new
-        while docs.has_next?
-          docs = @connection.paginate(docs.next_page, docs.per_page, "select", params: { q: query })["response"]["docs"]
-          docs.each do |doc|
-            obj = query_service.find_by(id: doc['id'])
-            obj.preservation_event_ids.split(',').each { |id| yield id }
-          end
+      def initialize(query_service:)
+        @query_service = query_service
+        @solr_service = Hyrax::SolrService
+      end
+
+      attr_reader :query_service
+
+      ##
+      # @returns Array[PreservationEvents ids] or []
+      def find_all_associated_preservation_event_ids
+        return_array = []
+        valkyrie_objects.each do |obj|
+          obj.preservation_event_ids.split(',').each { |id| return_array += [id] } if obj.preservation_event_ids.present?
         end
+        return_array
+      end
+
+      def solr_documents
+        @solr_service
+          &.query_result(query, fl: fields_selection, rows: 10_000_000)
+          &.[]('response')
+          &.[]('docs')
+      end
+
+      def valkyrie_objects
+        solr_documents.map { |doc| query_service.find_by(id: doc['id']) }
       end
 
       # Solr query for metadata objects with preservation_events_tesim values present.
       # @return query string.
       def query
         "preservation_events_tesim:*"
+      end
+
+      # Field(s) that the search results are restricted to
+      # @return String
+      def fields_selection
+        "id"
       end
     end
   end
