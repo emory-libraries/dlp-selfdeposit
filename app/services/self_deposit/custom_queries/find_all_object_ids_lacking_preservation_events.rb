26931e366318e8aef640a8ed9540880fc0c4ee5f
Brad Watson <brad.watson.orlando@gmail.com>
2025-06-03 07:30:42 -0500

    Switches FindAllObjectIdsLackingPreservationEvents to Hyrax::SolrService. (#1038)

--- app/services/self_deposit/custom_queries/find_all_object_ids_lacking_preservation_events.rb
+++ app/services/self_deposit/custom_queries/find_all_object_ids_lacking_preservation_events.rb
@@ -6,49 +6,50 @@ module SelfDeposit
     #   Hyrax.custom_queries.find_all_object_ids_lacking_preservation_events
     #
     # @see https://github.com/samvera/valkyrie/wiki/Queries#custom-queries
-    class FindAllObjectIdsLackingPreservationEvents < SolrDocumentMultipleReturnQuery
-      self.queries = [:find_all_object_ids_lacking_preservation_events]
+    class FindAllObjectIdsLackingPreservationEvents
+      def self.queries
+        [:find_all_object_ids_lacking_preservation_events]
+      end
+
+      def initialize(query_service:)
+        @query_service = query_service
+        @solr_service = Hyrax::SolrService
+      end
+
+      attr_reader :query_service
 
       ##
       # @returns [Publications#id, FileSets#id]
       def find_all_object_ids_lacking_preservation_events(model: nil)
         @model = model
-        enum_for(:each)
+        valkyrie_objects_from_filter_query
       end
 
-      # Queries the Solr index for Publication or FileSet (or specified model) object ids
-      #   lacking an association to the PreservationEvent model.
-      # For each Document, it yields the pulled object's ID string.
-      # @yield ID string
-      def each
-        docs = Valkyrie::Persistence::Solr::Queries::DefaultPaginator.new
-        while docs.has_next?
-          docs = @connection.paginate(
-            docs.next_page,
-            docs.per_page,
-            "select",
-            params: {
-              q: query,
-              fq: @model.present? ? model_filter_query : default_filter_query
-            }
-          )["response"]["docs"]
-          docs.each { |doc| yield doc['id'] }
-        end
+      def solr_documents_with_filter_query
+        @solr_service
+          &.query_result(query, fl: fields_selection, fq: filter_query, rows: 10_000_000)
+          &.[]('response')
+          &.[]('docs')
       end
 
-      # Solr query for Publication or FileSet (or specified model) objects with
-      #   no preservation_events_tesim values.
-      # @return query string.
+      def valkyrie_objects_from_filter_query
+        solr_documents_with_filter_query.map { |doc| query_service.find_by(id: doc['id']) }
+      end
+
+      # Solr query for objects with no preservation_events_tesim values.
+      # @return String.
       def query
         "-preservation_events_tesim:['' TO *]"
       end
 
-      def default_filter_query
-        "has_model_ssim:Publication || has_model_ssim:FileSet"
+      # Solr filter query for either a certain class name or either Publication or FileSet, based on whether model was passed
+      # @return String
+      def filter_query
+        @model.present? ? "has_model_ssim:#{@model}" : "has_model_ssim:Publication || has_model_ssim:FileSet"
       end
 
-      def model_filter_query
-        "has_model_ssim:#{@model}"
+      def fields_selection
+        "id"
       end
     end
   end
