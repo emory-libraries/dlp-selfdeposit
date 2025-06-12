803eefaf4dedc3d73994d1861ce924e26c99d3de
bwatson78 <brad.watson.orlando@gmail.com>
2025-06-12 07:48:25 -0500

    first round of logic upgrade work, all tests pass locally.

---
+++ config/initializers/hyrax_simple_schema_loader_override.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# [Hyrax-overwrite-v5.1.0] -set collection type to be ordered when `ordered: true` is set in the configuration
+
+Rails.application.reloader.to_prepare do
+  Hyrax::SimpleSchemaLoader::AttributeDefinition.class_eval do
+    # @return [Dry::Types::Type]
+    def type
+      collection_type = if config['multiple']
+                          Valkyrie::Types::Array.constructor { |v| Array(v).select(&:present?) }
+                        else
+                          Hyrax::SimpleSchemaLoader::AttributeDefinition::Identity
+                        end
+
+      config['ordered'] ? collection_type.of(type_for(config['type'])).meta(ordered: true) : collection_type.of(type_for(config['type']))
+    end
+  end
+end
