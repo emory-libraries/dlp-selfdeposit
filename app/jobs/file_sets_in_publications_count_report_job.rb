9e3202ff06021771da14ca3fb15a53dd69bdb366
bwatson78 <brad.watson.orlando@gmail.com>
2025-06-27 11:06:31 -0500

    makes query fully solr.

--- app/jobs/file_sets_in_publications_count_report_job.rb
+++ app/jobs/file_sets_in_publications_count_report_job.rb
@@ -2,13 +2,13 @@
 
 class FileSetsInPublicationsCountReportJob < Hyrax::ApplicationJob
   def perform
-    publications = Hyrax.query_service.custom_queries.find_all_by_model_via_solr(model: Publication)
+    counts = Hyrax.query_service.custom_queries.find_file_set_counts_for_all_publications
     csv_path = Rails.root.join('tmp', 'emory', "file_sets_in_publications_count_#{DateTime.now.strftime('%Y%m%dT%H%M')}.csv")
 
     ::FileUtils.mkdir_p(File.dirname(csv_path))
     CSV.open(csv_path, "w") do |csv|
       csv << ['PID', 'FileSet count']
-      publications.each { |pub| csv << [pub.deduplication_key, pub.member_ids.count] }
+      counts.each { |arr| csv << arr }
     end
   end
 end
