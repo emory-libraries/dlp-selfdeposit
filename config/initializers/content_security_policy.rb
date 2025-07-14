bfa7b230ef05bc57bb3558827cbccaf18d0e53c1
bwatson78 <brad.watson.orlando@gmail.com>
2025-07-14 09:25:12 -0500

    removes CSP rules.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,7 +10,7 @@ Rails.application.config.content_security_policy do |policy|
   #   policy.font_src    :self, :https, :data
   #   policy.img_src     :self, :https, :data
   #   policy.object_src  :none
-  policy.script_src :self, :https, :unsafe_eval, :unsafe_inline
+  #   policy.script_src :self, :https, :unsafe_eval, :unsafe_inline
   #   policy.style_src   :self, :https
 
   #   # Specify URI for violation reports
