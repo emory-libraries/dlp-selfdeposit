7214eadef158017fe1486462da0751e2ea27f122
bwatson78 <brad.watson.orlando@gmail.com>
2025-07-15 11:17:00 -0500

    Changes the unknown user redirect set by Hyrax.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,21 @@ class ApplicationController < ActionController::Base
   with_themed_layout '1_column'
 
   protect_from_forgery with: :exception
+
+  private
+
+  # Hyrax v5.0.1 override: changes html redirect to our saml path
+  def deny_access_for_anonymous_user(exception, json_message)
+    session['user_return_to'] = request.url
+    respond_to do |wants|
+      wants.html do
+        if AuthConfig.use_database_auth?
+          redirect_to main_app.new_user_session_path, alert: exception.message
+        else
+          redirect_to main_app.user_saml_omniauth_authorize_path, alert: exception.message
+        end
+      end
+      wants.json { render_json_response(response_type: :unauthorized, message: json_message) }
+    end
+  end
 end
