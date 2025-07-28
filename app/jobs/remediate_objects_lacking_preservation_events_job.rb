9dea8f6d18cede52401a8c5e4f548cac031a1ea5
Brad Watson <brad.watson.orlando@gmail.com>
2025-07-28 08:55:37 -0500

    Rescues and announces Publications not found. (#1177)

--- app/jobs/remediate_objects_lacking_preservation_events_job.rb
+++ app/jobs/remediate_objects_lacking_preservation_events_job.rb
@@ -5,19 +5,29 @@ class RemediateObjectsLackingPreservationEventsJob < Hyrax::ApplicationJob
   include PreservationEvents
 
   def perform
-    query_service = Hyrax.query_service
-    publication_ids_lacking_events = query_service&.custom_queries
-                                                  &.find_all_object_ids_lacking_preservation_events(model: Publication)
-                                                  &.to_a&.compact&.uniq
+    publication_ids_lacking_events = Hyrax.query_service
+                                          &.custom_queries
+                                          &.find_all_object_ids_lacking_preservation_events(model: Publication)
+                                          &.to_a&.compact&.uniq
 
-    publication_ids_lacking_events.each do |id|
-      publication = query_service.find_by(id:)
+    process_lacking_publications(publication_ids: publication_ids_lacking_events)
+  end
+
+  private
+
+  def process_lacking_publications(publication_ids:)
+    publication_ids.each do |id|
+      Rails.logger.info("Processing Preservation Events for Publication with ID #{id}.")
+      publication = Hyrax.query_service.find_by(id:)
       event_start = publication.created_at
       user_email = publication.depositor
 
       create_preservation_event(publication, work_creation(event_start:, user_email:))
       create_preservation_event(publication, work_policy(event_start:, visibility: publication.visibility, user_email:))
       Rails.logger.info("Preservation Events for Publication with ID #{id} have been remediated.")
+    rescue
+      Rails.logger.error("A Publication with ID #{id} was not found.")
+      next
     end
   end
 end
