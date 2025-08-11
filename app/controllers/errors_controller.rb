81af7f3debed1b4079b1d15da439453f3f1b3e49
Brad Watson <brad.watson.orlando@gmail.com>
2025-08-11 09:11:19 -0500

    tweak. (#1203)
    Co-authored-by: azotov <alexandr.zotov@emory.edu>

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 class ErrorsController < ApplicationController
+  def not_found
+    set_locale
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = :en
+  end
 end
